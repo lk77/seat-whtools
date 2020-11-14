8256651eb0163a99d05daa54de24b1161d66a7d2
Mathias E <mathias.eudeline@gmail.com>
2020-11-14 20:43:45 +0100

    Fixing whtools certificate page for admin user

--- src/resources/views/corporationCertificates.blade.php
+++ src/resources/views/corporationCertificates.blade.php
@@ -33,117 +33,119 @@
         </div>
     </div>
 </div>
-@push('javascript')
-    <script type="application/javascript">
+@if(auth()->user()->main_character->affiliation->corporation->entity_id)
+    @push('javascript')
+        <script type="application/javascript">
 
-        var corpCertTable = $('#corpCertTable').DataTable();
-        $(function () {
-            populateCorporationCertificates('{{auth()->user()->main_character->affiliation->corporation->entity_id}}');
-        });
+            var corpCertTable = $('#corpCertTable').DataTable();
+            $(function () {
+                populateCorporationCertificates('{{auth()->user()->main_character->affiliation->corporation->entity_id}}');
+            });
 
-        function populateCorporationCertificates(corpID) {
-            $.ajax({
-                headers: function () {
-                },
-                url: "/whtools/getcorpcert/" + corpID,
-                type: "GET",
-                dataType: 'json',
-                timeout: 10000
-            }).done(function (result) {
-                let headerPopulated;
-                if (result) {
-                    $('#corpCertTable').find("tbody").empty();
-                    if (corpCertTable) {
-                        corpCertTable.destroy();
+            function populateCorporationCertificates(corpID) {
+                $.ajax({
+                    headers: function () {
+                    },
+                    url: "/whtools/getcorpcert/" + corpID,
+                    type: "GET",
+                    dataType: 'json',
+                    timeout: 10000
+                }).done(function (result) {
+                    let headerPopulated;
+                    if (result) {
+                        $('#corpCertTable').find("tbody").empty();
+                        if (corpCertTable) {
+                            corpCertTable.destroy();
+                        }
+
+                        headerPopulated = false;
+                        for (var character in result) {
+                            row = "<tr>";
+                            row = row + "<td>" + result[character]['0'].character_name + "</td>";
+                            for (var certificate in result[character]) {
+                                row = row + "<td>" + drawStars(result[character][certificate].rank, true) + "</td>";
+                                row = row + "<td>" + (result[character][certificate].rank > 4 ? 1 : 0) + "</td>";
+
+                                // populate header and footer only once
+                                if (!headerPopulated) {
+                                    $('#corpCertTable').find("thead").find("tr").append("<th>" + result[character][certificate].cert_name + "</th>");
+                                    $('#corpCertTable').find("thead").find("tr").append("<th></th>");
+                                    $('#corpCertTable').find("tfoot").find("tr").append("<td></td>");
+                                    $('#corpCertTable').find("tfoot").find("tr").append("<td></td>");
+                                }
+                            }
+                            row = row + "</tr>";
+
+                            headerPopulated = true;
+                            $('#corpCertTable').find("tbody").append(row);
+                        }
                     }
 
-                    headerPopulated = false;
-                    for (var character in result) {
-                        row = "<tr>";
-                        row = row + "<td>" + result[character]['0'].character_name + "</td>";
-                        for (var certificate in result[character]) {
-                            row = row + "<td>" + drawStars(result[character][certificate].rank, true) + "</td>";
-                            row = row + "<td>" + (result[character][certificate].rank > 4 ? 1 : 0) + "</td>";
+                    corpCertTable = $('#corpCertTable').DataTable({
+                        "scrollX": true,
+                        "footerCallback": function (row, data, start, end, display) {
+                            var api = this.api(), data;
 
-                            // populate header and footer only once
-                            if (!headerPopulated) {
-                                $('#corpCertTable').find("thead").find("tr").append("<th>" + result[character][certificate].cert_name + "</th>");
-                                $('#corpCertTable').find("thead").find("tr").append("<th></th>");
-                                $('#corpCertTable').find("tfoot").find("tr").append("<td></td>");
-                                $('#corpCertTable').find("tfoot").find("tr").append("<td></td>");
+                            // Remove the formatting to get integer data for summation
+                            var intVal = function (i) {
+                                return typeof i === 'string' ?
+                                    i.replace(/[\$,]/g, '') * 1 :
+                                    typeof i === 'number' ?
+                                        i : 0;
+                            };
+                            var populateFooters = function (col) {
+                                // Total over all pages
+                                total = api
+                                    .column(col)
+                                    .data()
+                                    .reduce(function (a, b) {
+                                        return intVal(a) + intVal(b);
+                                    }, 0);
+
+                                // Total over this page
+                                pageTotal = api
+                                    .column(col, {page: 'current'})
+                                    .data()
+                                    .reduce(function (a, b) {
+                                        return intVal(a) + intVal(b);
+                                    }, 0);
+
+                                // Update footer
+                                $(api.column(col - 1).footer()).html(
+                                    pageTotal + ' (' + total + ' total)'
+                                );
+                            }
+                            numCol = parseInt($('#corpCertTable thead th').length);
+                            for (i = 0; i < numCol; i++) {
+                                if (i > 0 && (i % 2 == 0)) {
+                                    populateFooters(i);
+                                    api.column(i).visible(false);
+                                }
                             }
                         }
-                        row = row + "</tr>";
+                    });
+                });
+                ids_to_names();
+            }
 
-                        headerPopulated = true;
-                        $('#corpCertTable').find("tbody").append(row);
-                    }
-                }
-
-                corpCertTable = $('#corpCertTable').DataTable({
-                    "scrollX": true,
-                    "footerCallback": function (row, data, start, end, display) {
-                        var api = this.api(), data;
-
-                        // Remove the formatting to get integer data for summation
-                        var intVal = function (i) {
-                            return typeof i === 'string' ?
-                                i.replace(/[\$,]/g, '') * 1 :
-                                typeof i === 'number' ?
-                                    i : 0;
-                        };
-                        var populateFooters = function (col) {
-                            // Total over all pages
-                            total = api
-                                .column(col)
-                                .data()
-                                .reduce(function (a, b) {
-                                    return intVal(a) + intVal(b);
-                                }, 0);
-
-                            // Total over this page
-                            pageTotal = api
-                                .column(col, {page: 'current'})
-                                .data()
-                                .reduce(function (a, b) {
-                                    return intVal(a) + intVal(b);
-                                }, 0);
-
-                            // Update footer
-                            $(api.column(col - 1).footer()).html(
-                                pageTotal + ' (' + total + ' total)'
-                            );
-                        }
-                        numCol = parseInt($('#corpCertTable thead th').length);
-                        for (i = 0; i < numCol; i++) {
-                            if (i > 0 && (i % 2 == 0)) {
-                                populateFooters(i);
-                                api.column(i).visible(false);
+            $.get("{{ route('whtools.certCoverageChart', auth()->user()->main_character->affiliation->corporation->entity_id) }}", function (data) {
+                new Chart($('canvas#skills-coverage'), {
+                    type: 'radar',
+                    data: data,
+                    options: {
+                        scale: {
+                            ticks: {
+                                beginAtZero: true,
+                                max: 100
                             }
+                        },
+                        legend: {
+                            display: false
                         }
                     }
                 });
             });
-            ids_to_names();
-        }
-
-        $.get("{{ route('whtools.certCoverageChart', auth()->user()->main_character->affiliation->corporation->entity_id) }}", function (data) {
-            new Chart($('canvas#skills-coverage'), {
-                type: 'radar',
-                data: data,
-                options: {
-                    scale: {
-                        ticks: {
-                            beginAtZero: true,
-                            max: 100
-                        }
-                    },
-                    legend: {
-                        display: false
-                    }
-                }
-            });
-        });
-    </script>
-    @include('web::includes.javascript.id-to-name')
-@endpush
+        </script>
+        @include('web::includes.javascript.id-to-name')
+    @endpush
+@endif
